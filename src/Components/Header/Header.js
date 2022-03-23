0f534e660ecd04cf13814f968b93f5c097f0f22f
Dipok Roy <roydipok022@gmail.com>
2022-03-24 00:17:28 +0600

    hero section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Header.css";
 import images from "../../images/Logo.svg";
+import HeroSection from "../HeroSection/HeroSection";
 
 const Header = () => {
   return (
-    <div className="header">
-      <img src={images} alt="" />
-      <div className="menu">
-        <a href="/order">Order</a>
-        <a href="/order Review">Order Review</a>
-        <a href="/manage Inventory">Manage Inventory</a>
-        <a href="/about">About</a>
+    <div>
+      <div className="header">
+        <img src={images} alt="" />
+        <div className="menu">
+          <a href="/order">Order</a>
+          <a href="/order Review">Order Review</a>
+          <a href="/manage Inventory">Manage Inventory</a>
+          <a href="/about">About</a>
+        </div>
       </div>
+      <HeroSection></HeroSection>
     </div>
   );
 };
---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div>
+        <h1>Hero info</h1>
+      </div>
+      <div>
+        <h1>Hero image</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
