b4945397a93824d0967bdc632bba7c4c8ecafb4d
Dipok Roy <roydipok022@gmail.com>
2022-03-24 01:17:36 +0600

    fetch products

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 
 const Shop = () => {
   const [cart, setCart] = useState([]);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div>
       <h1>Welcome my ema john shopping</h1>
