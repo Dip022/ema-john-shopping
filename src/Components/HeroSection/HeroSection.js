3a68d4d9d957241dd09d3a6e5d0f9650d824ed94
Dipok Roy <roydipok022@gmail.com>
2022-03-24 00:43:45 +0600

    hero info added

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <div>
-        <h1>Hero info</h1>
+      <div className="hero-info">
+        <p className="discount">Sale up to 70% off</p>
+        <h1>New Collection For Fall</h1>
+        <p>Discover all the new arrivals of ready-to-wear collection.</p>
+        <button>SHOP NOW</button>
       </div>
       <div>
         <h1>Hero image</h1>
