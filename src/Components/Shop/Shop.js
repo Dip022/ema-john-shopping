6c663f8a1e2480c24596b5c128689b3e532ad07f
Dipok Roy <roydipok022@gmail.com>
2022-03-24 01:07:27 +0600

    create  shop component

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Shop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome my ema john shopping</h1>
+    </div>
+  );
 };
 
 export default Shop;
