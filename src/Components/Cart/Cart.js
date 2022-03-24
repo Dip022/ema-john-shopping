786d7d0343ed038a7d60cc559c7096ce37679931
Dipok Roy <roydipok022@gmail.com>
2022-03-24 15:26:36 +0600

    products object destructuring

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Cart = ({ product }) => {
   console.log(product.name);
+  const { img, name, price, seller, ratings } = product;
   return (
     <div>
       <h1>Name: {product.name}</h1>
