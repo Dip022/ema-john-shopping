ede68167e6ff27864dd624e1be9619da05f71cf6
Dipok Roy <roydipok022@gmail.com>
2022-03-24 17:08:48 +0600

    products added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ product }) => {
   console.log(product.name);
   const { img, name, price, seller, ratings } = product;
   return (
-    <div>
-      <h1>Name: {product.name}</h1>
+    <div className="cart">
+      <div className="product-info">
+        <img src={img} alt="" />
+        <p className="product-name">{name}</p>
+        <p className="product-price">
+          Price: <span>$</span>
+          {price}
+        </p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} start</p>
+      </div>
     </div>
   );
 };
