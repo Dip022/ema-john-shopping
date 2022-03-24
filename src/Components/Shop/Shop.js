75590818a2249a5224db510b3eb84d80144a88ac
Dipok Roy <roydipok022@gmail.com>
2022-03-24 15:04:33 +0600

    products summary added

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./Shop.css";
 
 const Shop = () => {
   const [cart, setCart] = useState([]);
@@ -8,8 +9,13 @@ const Shop = () => {
       .then((data) => console.log(data));
   }, []);
   return (
-    <div>
-      <h1>Welcome my ema john shopping</h1>
+    <div className="shop">
+      <div className="products">
+        <h1>Product list</h1>
+      </div>
+      <div className="order-summary">
+        <h1>order summary</h1>
+      </div>
     </div>
   );
 };
